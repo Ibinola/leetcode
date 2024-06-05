145bdc282a238ae94c3209c4222b343ee6b423da
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-04 19:48:16 -0700

    Time: 54 ms (84.76%), Space: 17.7 MB (38.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,11 @@
-# ````
-# [2,7,11,15] target = 9 -> [0, 1]
-# 
-#  {2: 0, 7: 1, 11: 2, 15: 3}
-#  2 - 9 = 7
-# 7 
-# ````
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        number_map = {}
+        nums_map = {}
 
         for i, number in enumerate(nums):
-            number_map[number] = i #{3: 0,2: 1,4: 2]}
-        
-        for i, number in enumerate(nums): #3
-            compliment = target - number # 6 - 2 = 4
-            if compliment in number_map and number_map[compliment] != i:
-                return [i, number_map[compliment]]
-        
+            nums_map[number] = i
 
-        
+        for i, number in enumerate(nums):
+            number_needed = target - number #[2,7,11,15] -> 6
+            if number_needed in nums_map and nums_map[number_needed] != i:
+                return [i, nums_map[number_needed]]
