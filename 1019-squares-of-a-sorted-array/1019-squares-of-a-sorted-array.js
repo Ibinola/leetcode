4bd1807bb80b983b1624de80648aece4f25f1980
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-07 18:58:02 +0100

    Time: 91 ms (29.24%), Space: 56.8 MB (15.78%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+   return nums.map((num) => {
+        return num * num
+    }).sort((a, b) => a - b)
+
+    
+};
