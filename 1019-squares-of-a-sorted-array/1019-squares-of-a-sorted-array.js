6cfef9adb201a81fbec94affb9d6cbca581290fc
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-07 19:05:28 +0100

    Time: 133 ms (8.56%), Space: 62.2 MB (5.18%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -1,17 +1,57 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// var sortedSquares = function(nums) {
+//    return nums.map((num) => {
+//         return num * num
+//     }).sort((a, b) => a - b)
+
+//     // const num = []
+//     // for (let i = 0; i < nums.length; i++){
+//     //      num.push(nums[i] * nums[i])
+//     // }
+
+//     // return num.sort((a, b) => a - b)
+    
+// };
+
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-   return nums.map((num) => {
-        return num * num
-    }).sort((a, b) => a - b)
+    let ans=[];
+for(let i=0;i<nums.length;i++){
+    ans.push(nums[i]*nums[i]);
+}
 
-    // const num = []
-    // for (let i = 0; i < nums.length; i++){
-    //      num.push(nums[i] * nums[i])
-    // }
 
-    // return num.sort((a, b) => a - b)
+function mergeSort(arr) {
+   if (arr.length <= 1) {
+      return arr;
+   }
+   const mid = Math.floor(arr.length / 2);
+   const left = arr.slice(0, mid);
+   const right = arr.slice(mid);
+   return merge(mergeSort(left), mergeSort(right));
+}
+ 
+//function to merge the left and right elements
+function merge(left, right) {
+   const result = [];
     
+   while (left.length && right.length) {
+      if (left[0] < right[0]) {
+         result.push(left.shift());
+      } else {
+         result.push(right.shift());
+      }
+   }
+    
+   return [...result, ...left, ...right];
+}
+
+ans=(mergeSort(ans));
+return(ans)
 };
