87b72c113421e347aa8dee512c51c0224ab8d84a
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-11 10:42:27 +0100

    Time: 35 ms (95.24%), Space: 16.9 MB (97.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+'''
+"A man, a plan, a canal: Panama"
+'''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
