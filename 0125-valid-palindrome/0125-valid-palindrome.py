71b9b5dda608b8cd56dd4caa83034d651a2d6990
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-11 10:59:17 +0100

    Time: 45 ms (55.68%), Space: 17 MB (59.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,6 +6,6 @@ class Solution:
         newStr = ''
 
         for c in s:
-            if c.isalnum():
+            if c.isalpha() or c.isdigit():
                 newStr += c.lower()
         return newStr == newStr[::-1]
