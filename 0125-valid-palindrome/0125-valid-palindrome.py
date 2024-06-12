7acefa6afa066eebd08a449e553b304cf85003cb
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-12 19:26:57 +0100

    Time: 49 ms (33.5%), Space: 17 MB (59.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,29 @@
 '''
 "A man, a plan, a canal: Panama"
+ i                            j
+
+ amanaplanacanalpanama
+  i                j
+ raceacar
+
+ newstr = 'amanapaanaaaaa'
+
+ - iterate through c in str
+  - if c isa():
+        - newstr".lower()
+
+- return newtrs == new.reverse()[::-1]
+
+race a car
+
 '''
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ''
+        newStr = '' #a
 
-        for c in s:
-            if c.isalpha() or c.isdigit():
+        for c in s: #A man, a plan, a canal: Panama
+            if c.isalnum():
                 newStr += c.lower()
+        
         return newStr == newStr[::-1]
