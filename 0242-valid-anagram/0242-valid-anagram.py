4cf6a5db7e1491e704d629a85256a5dc5bd6b428
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-16 23:34:27 +0100

    Time: 54 ms (31.16%), Space: 17.2 MB (27.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,12 @@
 '''
-if they are not the same length, return false
+can i sort, aplhabeticallu ?
+'aaagram'
 
-anagram -> {a:2, n: 1, g: 1, r: 1, m: 1}
-nagaram -> {a: 2, n: 1, g: 1, r: 1, m: 1}
-
-rat -> {r: 1, a: 1, t: 1}
-car -> {c: 1, a: 1, r: 1}
-
-time complexity - O(n + m)
-space complexity - O(n)
 '''
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        countS, countT = {}, {} # {r: 1, a: 1}
+        sorted_s, sorted_t = ''.join(sorted(s)), ''.join(sorted(t))
 
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+        return sorted_s == sorted_t
+
