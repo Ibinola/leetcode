7ecb033ccffab90759c9136f86681fdc2a542648
Michael Ibinola <ibinolamichael1@gmail.com>
2024-06-21 19:58:34 +0100

    Time: 41 ms (15.83%), Space: 16.5 MB (21.08%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -15,17 +15,9 @@ class Solution:
         slow = head
         fast = head
 
-        while fast.next and fast.next.next != None:
+        while fast and fast.next:
+            slow = slow.next
             fast = fast.next.next
-            slow = slow.next
-        
-        if fast.next != None and fast.next.next == None:
-            slow = slow.next
-            fast = fast.next
-        
-        # while slow.next != None:
-        #     output.append(slow.val)
-        #     slow = slow.next
-        # output.append(fast.val)
         
         return slow
+    
